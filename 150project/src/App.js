e70932513b41e2a188be45ce9ae013049f9d22b0
Erika Nepomuceno <eanepomuceno@up.edu.ph>
2019-05-15 23:23:07 +0800

    Nav bar lol

---
+++ 150project/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Nav">
+        <ul>
+          <li><a href="default.asp">Simplex</a></li>
+          <li><a href="news.asp">Quadratic Spline Interpolation</a></li>
+          <li><a href="news.asp">Polynomial Regression</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
